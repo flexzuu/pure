fefcf1d8700f0736895817015eb3dc3846a4990d
Jonas Faber <dev.jonas.faber@gmail.com>
2019-03-02 17:37:45 +0100

    Update fish_right_prompt.fish

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -1,5 +1,5 @@
 function fish_right_prompt \
     --description "Let user override prompt"
-
+    set --local pure_right_prompt (_pure_right_promt_kubectl_status)
     printf "%s%s%s" $pure_color_right_prompt "$pure_right_prompt" $pure_color_normal
 end
